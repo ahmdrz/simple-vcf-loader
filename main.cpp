8a1cb923d292f835b560ee726edf004c9d2e2a11
Ahmadreza Zibaei <ahmdrz@users.noreply.github.com>
2016-11-01 15:51:22 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -298,6 +298,8 @@ int main()
                                             item.mobile = line;
                                         }
                                     }
+                                    else                                    
+                                        item.mobile = "-";                                    
                                 }
                             }
 
